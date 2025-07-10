82e71fa2d882a111e0ce5e67ab067e6644db44dc
facundo enriquez <facundo.tomas.enriquez@gmail.com>
2025-07-09 21:32:04 -0300

    adding functions in service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,10 +5,15 @@ import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+  constructor(@InjectRepository(User) private repo: Repository<User>) { }
 
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
+
     return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
   }
 }
